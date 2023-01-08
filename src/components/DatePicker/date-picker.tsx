63306987131ee5d52b0764d3e30c6570efc5fbb1
Viktoriia <v@gaynulina.ru>
2023-01-08 20:41:20 +0100

    DatePicker: 1) added default dates set-up from the route; 2) added Search=ArtistsIDs support

--- src/components/DatePicker/date-picker.tsx
+++ src/components/DatePicker/date-picker.tsx
@@ -3,29 +3,50 @@ import b from 'b_';
 import { DatePicker } from 'antd';
 import { useNavigate, useParams } from 'react-router-dom';
 import { RangeValue } from 'rc-picker/es/interface';
-import dayjs, { Dayjs } from 'dayjs';
+import { Dayjs } from 'dayjs';
 const { RangePicker } = DatePicker;
 
 import './date-picker.scss';
 import { IUrlRouteParams, RouterPath } from '../../interfaces';
+import { transformDayjsString } from '../../pages/Date/date';
+import { getArtistsIdsQuery, useArtistsIDs } from '../../shared/artists-ids-hook';
 
 export const DatesPicker = () => {
   const navigate = useNavigate();
-  const { genres, mode, countries, evening, search } = useParams<IUrlRouteParams>();
+  const { genres, mode, countries, period } = useParams<IUrlRouteParams>();
 
-  const [startDate, setStartDate] = React.useState<string | null>('');
-  const [endDate, setEndDate] = React.useState<string | null>('');
+  const artistsIds = useArtistsIDs();
+  const [dateRange, setDateRange] = React.useState<[Dayjs, Dayjs] | null>(null);
 
   const dateFormat = 'DD/MM';
   const handleChange = (dates: RangeValue<Dayjs>, formatString: string[]) => {
     const start = Object.values(dates!)[0]?.format('YYMMDD');
-    setStartDate(start!);
     const end = Object.values(dates!)[1]?.format('YYMMDD');
-    setEndDate(end!);
     const url = `/${mode || RouterPath.ARTISTS}/${start}-${end}/${countries || 'all'}/${
       genres || 'all'
-    }`;
+    }/${getArtistsIdsQuery(artistsIds)}`;
     navigate(url);
   };
-  return <RangePicker className={b('input')} onChange={handleChange} format={dateFormat} />;
+
+  useEffect(() => {
+    if (period && period !== 'all') {
+      const [start, end] = period!.split('-'); // 221220-221220
+
+      const st = transformDayjsString(start);
+      const en = transformDayjsString(end, true);
+
+      setDateRange([st, en]);
+    } else {
+      setDateRange(null);
+    }
+  }, [period]);
+
+  return (
+    <RangePicker
+      className={b('input')}
+      onChange={handleChange}
+      format={dateFormat}
+      value={dateRange}
+    />
+  );
 };
